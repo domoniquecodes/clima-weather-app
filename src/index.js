d7c8de0e83ae422a913677138b0b59a9269bc900
domoniquecodes <domonique.wilson89@icloud.com>
2024-01-27 20:48:27 -0500

    Added the rest of the API forecast data & adjusted icons

--- src/index.js
+++ src/index.js
@@ -14,12 +14,13 @@ function refreshWeather(response) {
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}m/h`;
-
   temperatureElement.innerHTML = Math.round(temperature);
   iconElement.innerHTML = `<img
         src="${response.data.condition.icon_url}"
         class="weather-app-icon"
       />`;
+
+  getForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -34,11 +35,12 @@ function formatDate(date) {
     "Friday",
     "Saturday",
   ];
-
   let day = days[date.getDay()];
+
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
+
   return `${day} ${hours}:${minutes}`;
 }
 
@@ -55,34 +57,52 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thurs", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Philadelphia");
 
-function displayForecast() {
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+function getForecast(city) {
+  let apiKey = "b78bba14cabt32e0f423fb4886foc395";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=imperial`;
+  axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      ` 
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        ` 
         <div class="weather-forecast-day">
-              <div class="weather-forecast-date">${day}</div>
-              <div class="weather-forecast-icon">🌤️</div>
-                <div class="weather-forecast-temperatures">
+              <div class="weather-forecast-date">${formatDay(day.time)}</div>
+               <img src="${
+                 day.condition.icon_url
+               }" class="weather-forecast-icon" />
+              <div class="weather-forecast-temperatures">
                 <div class="weather-forecast-temperature">
-                <strong>18°</strong>
+                <strong>${Math.round(day.temperature.maximum)}°</strong>
                 </div>
-                <div class="weather-forecast-temperature">12°</div>
+                <div class="weather-forecast-temperature">${Math.round(
+                  day.temperature.minimum
+                )}°</div>
               </div>
         </div>
     `;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
 }
 
-displayForecast();
+searchCity("Philadelphia");
